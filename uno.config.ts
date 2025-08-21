9a8c0f5633079b7e6b8ad93a311336d9dd005243
SQsuqi <643352633@qq.com>
2025-08-21 20:03:53 +0800

    refactor(uno): 调整按钮样式并移除 presetUno
    - 移除了 btn 类中的 text-white 和 bg-pink-300 类，以适应风雪草样式
    - 删除了 presetUno 引入，避免与风雪草样式冲突

--- uno.config.ts
+++ uno.config.ts
@@ -3,13 +3,12 @@ import {
   defineConfig,
   presetAttributify,
   presetIcons,
-  presetUno,
   presetWind4,
 } from 'unocss'
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-pink-300 text-white cursor-pointer hover:bg-pink-500 disabled:hover:bg-gray-600 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-pink-300 cursor-pointer hover:bg-pink-500 disabled:hover:bg-gray-600 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-pink-500 !outline-none'],
   ],
   presets: [
@@ -20,6 +19,5 @@ export default defineConfig({
     ),
     presetAttributify(),
     presetWind4(),
-    presetUno(),
   ],
 })
